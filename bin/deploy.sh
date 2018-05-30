82ff87206a4b778035aff2d5f25df53c493fcad2
Peter Bengtsson <mail@peterbe.com>
2018-05-29 20:15:42 -0400

    echos

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,8 +4,11 @@ set -eo pipefail
 export REACT_APP_ABSOLUTE_API_URL=https://api.minimalcss.app/minimize
 yarn run build
 
+echo "Inserting CSP meta tag..."
 ./bin/insert_csp.js build/index.html
 
+echo "Making version.json..."
 ./bin/update_version.js > build/version.json
 
+echo "Running gh-pages..."
 gh-pages --add --dist build/
