ef6e0ad0fd0b074c5bb77f63ee0fd26bd78001cb
Peter Bengtsson <mail@peterbe.com>
2019-05-07 12:08:13 -0400

    Updates

---
+++ precache-manifest.219ba5a281acb4a29d753c6e92cba88c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551a1f9a0ddeb90c6c837c3d32ac0cc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c341d9b03fbc1ea67571",
+    "url": "/static/css/2.39a364ee.chunk.css"
+  },
+  {
+    "revision": "3b95303612be1e8d7b1f",
+    "url": "/static/css/main.b2bcca92.chunk.css"
+  },
+  {
+    "revision": "c341d9b03fbc1ea67571",
+    "url": "/static/js/2.9e4aa102.chunk.js"
+  },
+  {
+    "revision": "3b95303612be1e8d7b1f",
+    "url": "/static/js/main.7484cc95.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
