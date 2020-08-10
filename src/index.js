9e66429775594fff53643256b38d8297d58b460f
Peter Bengtsson <mail@peterbe.com>
2020-08-10 11:21:38 -0400

    upgrade everythingish

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bulma/css/bulma.css";
-// import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-// registerServiceWorker();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
